b2131ebdf9bb9528bb80b49921e028d71aa1e7ba
Avitosh Totaram <a.totaram@gmail.com>
2019-05-06 12:03:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,18 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarfs)# code an argument here
   # Your code here
+  i = 0 
+  while i < dwarfs.length
+  dwarfs.each do |name|
+    puts "#{i+1} #{name}"
+    i+=1
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.map! {|name| name.capatalize}
+  planeteer_calls.collect {|name| name + "!"}
+  
 end
 
 def long_planeteer_calls# code an argument here
